701b81ea6c25231fd68eae0236302f38d9f34339
Daniel Gallichan <dgallichan@yahoo.com>
2023-07-05 12:03:32 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,36 @@
+function turnOffServos () {
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo1)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo2)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo3)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo4)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo5)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo6)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo7)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo8)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo9)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo10)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo11)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo12)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo13)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo14)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo15)
+    kitronik_i2c_16_servo.servoOff(kitronik_i2c_16_servo.Servos.Servo16)
+}
 function UP1 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, minVal)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo13, maxVal)
-    basic.pause(1000)
+    basic.pause(200)
 }
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 4; index++) {
         WALK()
     }
+    turnOffServos()
 })
 function DOWN1 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo13, 90)
-    basic.pause(1000)
+    basic.pause(200)
 }
 function ROTATE () {
     UP1()
@@ -23,7 +42,7 @@ function ROTATE () {
 }
 function ROT1 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, 90)
-    basic.pause(1000)
+    basic.pause(200)
 }
 function WALK () {
     UP1()
@@ -34,66 +53,75 @@ function WALK () {
     DOWN2()
 }
 function FOR2 () {
-    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, 0)
-    basic.pause(1000)
+    // You would want this to be 0, but something gets stuck on the robot past ~60 degrees
+    kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, 60)
+    basic.pause(200)
 }
-// * Servo Assignments
+// * DG - Servo Assignments
 // 
-// * 1 -> FR
+// * 1 -> BL
 // 
 // * 2 -> FL
 // 
-// * 3 -> BR
+// * 13 -> FR
 // 
-// * 4 -> BL
+// * 14 -> BR
 // 
-// *
+// * 6 -> Middle Rotation (Right-hand servo)
 // 
-// * pos 0 = down/straight leg
-// 
-// * pos 1 = lift up
-// 
-// *
-// 
-// * 5 -> Middle Forward
-// 
-// * 6 -> Middle Rotation
+// * 10 -> Middle Forward/Backward (left-hand servo)
 input.onButtonPressed(Button.AB, function () {
 	
 })
 function UP2 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, minVal)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo14, maxVal)
-    basic.pause(1000)
+    basic.pause(200)
 }
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
         ROTATE()
     }
+    turnOffServos()
 })
 function DEFAULT () {
+    // // * DG - Servo Assignments
+    // //
+    // // * 1 -> BL
+    // //
+    // // * 2 -> FL
+    // //
+    // // * 13 -> FR
+    // //
+    // // * 14 -> BR
+    // //
+    // // * 6 -> Middle Rotation (Right-hand servo)
+    // //
+    // // * 10 -> Middle Forward/Backward (left-hand servo)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo1, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo13, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo14, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, 90)
+    // It seems you would want this to be set to 180 to keep the body straight, but actually seems to get in the way of the forward/backward motion
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, 90)
+    turnOffServos()
 }
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     DEFAULT()
 })
 function FOR1 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo10, maxVal)
-    basic.pause(1000)
+    basic.pause(200)
 }
 function ROT2 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo6, maxVal)
-    basic.pause(1000)
+    basic.pause(200)
 }
 function DOWN2 () {
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo2, 90)
     kitronik_i2c_16_servo.servoWrite(kitronik_i2c_16_servo.Servos.Servo14, 90)
-    basic.pause(1000)
+    basic.pause(200)
 }
 let minVal = 0
 let maxVal = 0
